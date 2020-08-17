b7719eda2344c4ef3b007e041e47cccb5af5d4cd
Anubhav Mishra <anubhav.worklinux@gmail.com>
2020-08-17 17:48:32 +0530

    remove revoked token from apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -5,7 +5,7 @@ module.exports = {
       url: 'https://api.github.com/graphql',
       // optional headers
       headers: {
-        authorization: 'Bearer fdc6f1774bcba44c96a7446fe06dd60a8d4ba535',
+        authorization: 'Bearer XXXX-PUT-YOUR-TOKEN-HERE-XXXX',
       },
       // optional disable SSL validation check
       skipSSLValidation: true,
